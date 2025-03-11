8e4498ce4823adc30542caf8af7402aa21655b35
Ayushman2312 <ayushmantiwari2312@gmail.com>
2025-03-11 14:48:29 +0530

    ii

--- news_app/admin.py
+++ news_app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(User)
+admin.site.register(communication)
 admin.site.register(Article)
 admin.site.register(BlogPost)
 admin.site.register(Magazine)
